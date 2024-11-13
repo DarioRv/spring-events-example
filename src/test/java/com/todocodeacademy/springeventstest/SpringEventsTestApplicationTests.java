418f77a09136b501a0a0e8d0cf6abe1f3a96f0ac
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-11-13 20:46:44 -0300

    Añadir archivos de ejemplo

---
+++ src/test/java/com/todocodeacademy/springeventstest/SpringEventsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.springeventstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
